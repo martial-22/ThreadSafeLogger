e6d460c7b2370a42e28d6e837e03cfb11dfb0c56
Alexey Martushev <a.a.martushev@gmail.com>
2026-07-18 10:38:44 +0700

    Implement ThreadSafeLogger

---
+++ BlockingQueue.h
@@ -0,0 +1,59 @@
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <optional>
+
+namespace std
+{
+
+template<typename T>
+class BlockingQueue
+{
+public:
+    bool push(T value)
+    {
+	    {
+		    lock_guard<mutex> lock(m_);
+			if (stop_)
+			{
+				return false;
+			}
+			data_.push(move(value));
+		}
+	    cv_.notify_one();
+	    return true;
+    }
+
+    optional<T> pop()
+    {
+	    unique_lock<mutex> lock(m_);
+	    cv_.wait(lock, [&]() {
+		    return stop_ || !data_.empty();
+	    });
+	    
+	    if (!data_.empty())
+	    {
+		    T item = move(data_.front());
+		    data_.pop();
+		    return item;
+	    }
+	    return nullopt;
+    }
+    
+    void shutdown()
+    {
+	    {
+		    lock_guard<mutex> lock(m_);
+		    stop_ = true;
+	    }
+	    cv_.notify_all();
+    }
+    
+private:
+	mutex m_;
+	condition_variable cv_;
+	queue<T> data_;
+	bool stop_ = false;
+};
+
+}
---
+++ ThreadSafeLogger.h
@@ -0,0 +1,43 @@
+#include <string>
+#include <fstream>
+
+#include "BlockingQueue.h"
+
+namespace std
+{
+
+class Logger
+{
+public:
+    explicit Logger(const filesystem::path& file)
+	    : ofs_(file)
+    {
+	    auto write = [this]()
+	    {
+		    while (auto msg = queue_.pop())
+		    {
+			    ofs_ << *msg;
+		    }
+	    };
+	    
+	    writer_ = thread(move(write));
+    }
+
+    ~Logger()
+    {
+	    queue_.shutdown();
+	    writer_.join();
+    }
+
+    bool log(string message)
+    {
+	    return queue_.push(move(message));
+    }
+
+private:
+	ofstream ofs_;
+    BlockingQueue<string> queue_;
+    thread writer_;
+};
+
+}
